1d35d37bceb351e915884eab3452201b969a8235
4rtHurB-1 <78410232+4rtHurB-1@users.noreply.github.com>
2021-05-26 21:51:40 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,3 @@
-# build and run olx-cron
-if [ ! -d "physics-api" ]; then
-  git clone https://github.com/4rtHurB-1/physics-api physics-api
-fi
-
-cd physics-api
 git pull origin master
 npm i
 pm2 delete physics-api
